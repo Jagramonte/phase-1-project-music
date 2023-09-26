09bbd39abfa72a85d8a92ecb618706942af2e33d
Joseph Agramonte <lifeinabundance314@gmail.com>
2023-09-26 16:08:25 -0400

    completed all deliverables

--- script.js
+++ script.js
@@ -3,10 +3,26 @@
 fetch("http://localhost:3000/Songs")
   .then((r) => r.json())
   .then((songArray) => {
+    // loadDisplay()
     songArray.forEach(renderSong);
   });
+//function loadDisplay() {
+  fetch("http://localhost:3000/Songs/1")
+  .then((r) => r.json())
+  .then((firstSong) => {
+    detailSongImg.src = firstSong.image
+    detailSongName.innerText = firstSong.name
+  })
+//}
 
+// style.css.disable = true 
 const form = document.getElementById("newsongform");
+
+//global variables
+const songNameContainer = document.getElementById("songname");
+const detailSongImg = document.getElementById("detailsongimage");
+const detailSongName = document.getElementById("detailsongname");
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -29,8 +45,6 @@ form.addEventListener("submit", (e) => {
 function renderSong(songObj) {
   const newLi = document.createElement("li");
 
-  const songNameContainer = document.getElementById("songname");
-
   const songName = songObj.name;
 
   songNameContainer.append(newLi);
@@ -38,13 +52,28 @@ function renderSong(songObj) {
   newLi.append(songName);
 
   newLi.addEventListener("click", () => {
-    const detailSongName = document.getElementById("detailsongname");
+  
 
     detailSongName.innerText = songObj.name;
 
-    const detailSongImg = document.getElementById("detailsongimage");
+    
     detailSongImg.src = songObj.image;
   });
 
   const videoUrl = document.getElementById("videourl");
 }
+
+document.addEventListener('keypress', (e) => {
+  if(e.key === '`') {
+    const body = document.body
+    body.style.backgroundColor = 'black'
+
+    songNameContainer.style.color='green'
+    form.style.color='green'
+
+    const textGetter = document.querySelectorAll('.text')
+    textGetter.forEach ((element) => {
+      element.style.color='green'
+    })
+  }
+})
